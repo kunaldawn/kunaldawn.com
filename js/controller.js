c5a4d4333c0429f4c38a71804fdb5b21cc870d52
kunaldawn <kunal.dawn#gmail.com>
2015-06-14 04:42:54 +0900

    UI Updates
    Switched from angular-material to bootstrap3 beacuse of
    scrolling issue on tab content on mobile devices.

--- js/controller.js
+++ js/controller.js
@@ -1,76 +1,38 @@
-var app = angular.module('profileApp', ['ngMaterial', 'ngSanitize', 'youtube-embed']);
+var app = angular.module('profileApp', ['angular-loading-bar', 'ngAnimate']);
 
-app.controller('ProfileCtrl', ['$scope', '$mdSidenav', '$http', '$timeout', '$mdMedia', function($scope, $mdSidenav, $http, $timeout, $mdMedia){
+app.controller('ProfileCtrl', ['$scope', '$http', function($scope, $http){
     $scope.data = [];
     $scope.content = [];
-    $scope.loading = true;
     $scope.copyright = [];
     $scope.name = "";
-    $scope.showMenu = false;
-    $scope.smallScreen = false;
+    $scope.selectedPage = 0;
 
-    $scope.$watch(function() { return $mdMedia('sm'); }, function(small) {
-      $scope.smallScreen = small;
-    });
+    $scope.loadPage = function(page, index) {
+      $('#collapse-1').removeClass('in');
+      $scope.selectedPage = index;
+      $scope.content = page;
+    };
 
-    $http.get('contents.json').then(function(res){
+    $http.get('contents.json').then(function(res) {
         $scope.data = res.data;
         $scope.name = $scope.data.name;
         angular.forEach($scope.data.pages, function(page) {
             if (page.default) {
-                $scope.content = page;
+                $scope.loadPage(page, 0)
             }
         });
         $scope.copyright = $scope.data.copyright;
-        $timeout(function() {
-            $scope.loading = false;
-        }, 3000);
     });
 
-    $scope.toggleSidenav = function(menuId) {
-      if (!($scope.showMenu || $scope.loading) || $scope.smallScreen) {
-        $mdSidenav(menuId).toggle();
-      }
-    };
-
-    $scope.loadPage = function(page) {
-        $scope.toggleSidenav('left');
-        $scope.content = page;
-    };
-
 }]);
 
-app.provider('markdownConverter', function () {
-    var opts = {};
-    return {
-      config: function (newOpts) {
-        opts = newOpts;
-      },
-      $get: function () {
-        return new Showdown.converter(opts);
-      }
-    };
+app.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeBar = true;
+    cfpLoadingBarProvider.includeSpinner = false;
+    cfpLoadingBarProvider.latencyThreshold = 0;
+  }
+])
+
+app.config(function($sceProvider) {
+  $sceProvider.enabled(false);
 });
-
-app.directive('markdown', ['$sanitize', 'markdownConverter', function ($sanitize, markdownConverter) {
-    return {
-      restrict: 'AE',
-      link: function (scope, element, attrs) {
-        if (attrs.btfMarkdown) {
-          scope.$watch(attrs.btfMarkdown, function (newVal) {
-            var html = newVal ? $sanitize(markdownConverter.makeHtml(newVal)) : '';
-            element.html(html);
-          });
-        } else {
-          var html = $sanitize(markdownConverter.makeHtml(element.text()));
-          element.html(html);
-        }
-      }
-    };
-}]);
-
-app.config(['markdownConverterProvider', function (markdownConverterProvider) {
-  markdownConverterProvider.config({
-    extensions: ['github']
-  });
-}]);
